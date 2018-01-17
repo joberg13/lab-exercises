f7edb84e76bbe9f153d12a70464f4480718871a6
Jeffrey Oberg <joberg13@uw.edu>
2018-01-16 17:42:22 -0800

    adds changes to R file made in lab

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -9,37 +9,72 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# This does not work as all elements of a vector must be of the same type
+# and it is also impossible to sum a number and a string
 
 install.packages("stringr")
+
+#library("stringr")
 
 my.line <- "Hey, hey, this is the library"
 
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# This does not work as the function str_length could not be found. 
+# The function could not be found as the "stringr" package had not been loaded, therefore it could not be used.
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# This line does not work as the argument, initial, is a variable that does not exist. 
+# This could be corrected by changing it to initials, which does exist.
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
 
+test.vector <- c(1, 2, 3)
+
+typeof(test.vector)
+
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
+
+CompareLength <- function(vector1, vector2){
+  num <- length(vector1) - length(vector2)
+  
+  if(num < 0){
+    num <- num * -1
+  }
+  
+  return (paste("The difference in lengths is ", num))
+}
 
 
 # Pass two vectors of different length to your `CompareLength` function
 
+v1 <- c(1:2)
+v2 <- c(1:3)
+
+CompareLength(v1, v2)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
+
+DescribeDifference <- function(vector1, vector2){
+  num <- length(vector1) - length(vector2)
+  
+  if(num < 0){
+    num <- num * -1
+    return (paste("Your second vector is longer by", num, "elements"))
+  }
+  
+  return (paste("Your first vector is longer by", num, "elements"))
+}
+
 
 # Pass two vectors to your `DescribeDifference` function
 
